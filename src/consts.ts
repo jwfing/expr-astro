75717521999ce5ad1adff95ae503ffc6545ea193
jwfing <jwfing@gmail.com>
2023-03-06 21:19:59 +0800

    chore

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '程序猿爸爸的日常';
+export const SITE_DESCRIPTION = '记录日常所思所感，与孩子共同成长的时光';
